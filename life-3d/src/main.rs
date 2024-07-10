c5069aa7225ebca034c2b80c8a3c028cb2c74575
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-07-10 15:50:34 -0400

    Enabled OpenGL debugging.

--- life-3d/src/main.rs
+++ life-3d/src/main.rs
@@ -1,9 +1,38 @@
+use std::{
+    ffi::{c_char, c_void, CStr},
+    ptr::null,
+};
+
 use glad_gl::gl;
 use glfw::Context;
 
-use life_3d::{shaders, shader_program_from_resources};
+use life_3d::{shader_program_from_resources, shaders};
+
+extern "system" fn opengl_debug_callback(
+    _source: gl::GLenum,
+    _error_type: gl::GLenum,
+    _id: std::ffi::c_uint,
+    _severity: gl::GLenum,
+    _length: gl::GLsizei,
+    message: *const c_char,
+    _user_param: *mut c_void,
+) {
+    let message = unsafe { CStr::from_ptr(message) };
+    eprintln!(
+        "[OPENGL ERROR]: {}",
+        message.to_str().unwrap_or("<INVALID UTF-8>")
+    );
+}
 
 fn main() {
+    let mut debug_opengl = false;
+
+    for args in std::env::args() {
+        if args == "--debug-opengl" {
+            debug_opengl = true;
+        }
+    }
+
     let mut glfw = glfw::init(|error, message| eprintln!("[GLFW ERROR {:?}]: {}", error, message))
         .expect("Failed to initialize GLFW");
 
@@ -12,6 +41,10 @@ fn main() {
     ));
     glfw.window_hint(glfw::WindowHint::ContextVersion(4, 6));
 
+    if debug_opengl {
+        glfw.window_hint(glfw::WindowHint::OpenGlDebugContext(true));
+    }
+
     let (mut window, events) = glfw
         .create_window(1280, 720, "Life 3D", glfw::WindowMode::Windowed)
         .expect("Failed to create the GLFW window.");
@@ -19,6 +52,22 @@ fn main() {
     window.make_current();
     glfw.set_swap_interval(glfw::SwapInterval::Sync(1));
     gl::load(|sym| glfw.get_proc_address_raw(sym));
+
+    if debug_opengl {
+        unsafe {
+            gl::Enable(gl::DEBUG_OUTPUT);
+            gl::Enable(gl::DEBUG_OUTPUT_SYNCHRONOUS);
+            gl::DebugMessageCallback(opengl_debug_callback, std::ptr::null());
+            gl::DebugMessageControl(
+                gl::DONT_CARE,
+                gl::DONT_CARE,
+                gl::DONT_CARE,
+                0,
+                null(),
+                gl::TRUE,
+            );
+        }
+    }
 
     let shader_program = shader_program_from_resources!(shaders::MAIN_VERT, shaders::MAIN_FRAG);
 
