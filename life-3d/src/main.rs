2bf7613aba9ec96250e2cc691a1752dd016f5249
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-07-10 14:36:31 -0400

    Created a basic window.

--- life-3d/src/main.rs
+++ life-3d/src/main.rs
@@ -1,3 +1,39 @@
+use glad_gl::gl;
+use glfw::Context;
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(|error, message| eprintln!("[GLFW ERROR {:?}]: {}", error, message))
+        .expect("Failed to initialize GLFW");
+
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+    glfw.window_hint(glfw::WindowHint::ContextVersion(4, 6));
+
+    let (mut window, events) = glfw
+        .create_window(1280, 720, "Life 3D", glfw::WindowMode::Windowed)
+        .expect("Failed to create the GLFW window.");
+
+    window.make_current();
+    glfw.set_swap_interval(glfw::SwapInterval::Sync(1));
+    gl::load(|sym| glfw.get_proc_address_raw(sym));
+
+    while !window.should_close() {
+        unsafe {
+            gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.swap_buffers();
+        glfw.poll_events();
+
+        for (_, event) in glfw::flush_messages(&events) {
+            match event {
+                glfw::WindowEvent::FramebufferSize(width, height) => unsafe {
+                    gl::Viewport(0, 0, width, height);
+                },
+                _ => {}
+            }
+        }
+    }
 }
