e4387fbd8d565eca77c8b80ccb390bafff421f62
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-07-12 14:32:33 -0400

    Implemented a basic 3-component vector.

---
+++ life-3d/src/math.rs
@@ -0,0 +1,26 @@
+use std::ops::Add;
+
+#[repr(C)]
+pub struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Vec3 {
+    fn new(x: f32, y: f32, z: f32) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
