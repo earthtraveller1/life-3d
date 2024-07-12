b2da703a315c6146e41eda75cd6c77d00d4b6605
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-07-12 15:41:14 -0400

    Made the Vec3 stuff public + added a 2 component Vector type.

--- life-3d/src/math.rs
+++ life-3d/src/math.rs
@@ -2,13 +2,13 @@ use std::ops::Add;
 
 #[repr(C)]
 pub struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 impl Vec3 {
-    fn new(x: f32, y: f32, z: f32) -> Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
         Vec3 { x, y, z }
     }
 }
@@ -21,6 +21,29 @@ impl Add for Vec3 {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
             z: self.z + rhs.z,
+        }
+    }
+}
+
+#[repr(C)]
+pub struct Vec2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Vec2 {
+    pub fn new(x: f32, y: f32) -> Vec2 {
+        Vec2 { x, y }
+    }
+}
+
+impl Add for Vec2 {
+    type Output = Vec2;
+    
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec2 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
         }
     }
 }
