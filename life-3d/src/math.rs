3be2228021e9e5a290c76f41a3e4bfb107cb6199
earthtraveller1 <liangyuezhao7@gmail.com>
2024-07-31 17:31:04 -0400

    Added the perspective projection matrix.

--- life-3d/src/math.rs
+++ life-3d/src/math.rs
@@ -238,6 +238,22 @@ impl Mat4 {
         }
     }
 
+    pub fn orthographic(left: f32, right: f32, top: f32, bottom: f32, far: f32, near: f32) -> Mat4 {
+        Mat4 {
+            data: [
+                [2.0 / (right - left), 0.0, 0.0, 0.0],
+                [0.0, 2.0 / (top - bottom), 0.0, 0.0],
+                [0.0, 0.0, -2.0 / (far - near), 0.0],
+                [
+                    -(right + left) / (right - left),
+                    -(top + bottom) / (top - bottom),
+                    -(far + near) / (far - near),
+                    1.0,
+                ],
+            ],
+        }
+    }
+
     pub fn translate(x: f32, y: f32, z: f32) -> Mat4 {
         Mat4 {
             data: [
